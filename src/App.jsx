431918dcda01ee196e3b165231c4f9ac6053b641
ezeiboboi <technerdworld01@gmail.com>
2023-07-06 17:18:23 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import Header from './components/header/Header'
+import Home from './components/home/Home'
+import About from './components/about/About'
+import Footer from './components/footer/Footer';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+    <Header/>
+    <Home/>
+    <About/>
+    <Footer/>
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <h4>@POMA</h4>
+    </div>
+  )
+}
+
+export default Footer
